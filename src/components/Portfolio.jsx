2872ea1e4b8c51d352fed3d24ba4ddc203da66ab
Praveen Raj <rajp03262@gmail.com>
2023-12-29 16:23:03 +0530

    bug of splleing

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -56,7 +56,7 @@ const Contact = () => {
 
   return (
     <div
-      name="contact"
+      name="portfolio"
       className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
